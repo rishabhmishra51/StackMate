aeb4c72fa583949fb836facc8be2b7ecc3ad7200
rishabhmishra51 <rishabhmishra7570@gmail.com>
2026-01-11 19:07:16 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,32 +1,21 @@
 const express=require("express")
 const app=express();
-const {adminAuth,userAuth} =require("./middlewares/auth")
-//handle Auth middleware for all get,post,patch ...
-app.use("/admin",adminAuth);
 
-app.use("/user",(req,res,next)=>{
-if(req.path === "/login"){
-  return next();
-}
-userAuth(req,res,next);
+app.get("/getUserData",(req,res)=>{
+  try{
+throw new Error("dsafhj");
+res.send("user data sent") 
+  }
+  catch(err){
+res.status(500).send("something went wrong 1");
+  }
+});
+
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    res.status(500).send("something went wrong");
+  }
 })
-
-app.get("/user/profile", (req, res) => {
-  res.send("User profile");
-});
-
-app.get("/user/orders", (req, res) => {
-  res.send("User orders");
-});
-
-
-
-app.get("/admin/getAlldata",(req,res)=>{
-  res.send("All data sent");
-});
-app.get("/admin/deleteUser",(req,res)=>{
-  res.send("a User deleted");
-});
 
 app.listen(3000,()=>{
      console.log("Server is successful listing on port 3000");    
