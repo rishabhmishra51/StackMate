e818726aa5e8285654f959e898a81ead249882a2
Rishabh Kumar Mishra <161306539+rishabhmishra51@users.noreply.github.com>
2026-01-13 17:59:32 +0530

    Clear MongoDB connection string in database.js
    Updated MongoDB connection string to be empty.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://NodewithMongoDB:8pkKSlfyMhMkatiO@nodewithmongodb.tdjsh4e.mongodb.net/StackMate?retryWrites=true&w=majority"
+    ""
   );
 };
 
