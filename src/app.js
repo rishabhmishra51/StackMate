3eeb8049d91f70853c79dc63e57f0652fba84391
rishabhmishra51 <rishabhmishra7570@gmail.com>
2026-01-19 22:49:28 +0530

    more api

--- src/app.js
+++ src/app.js
@@ -46,6 +46,32 @@ app.get("/feed",async(req,res)=>{
   res.status(400).send("Something went wrong!")
  }
 })
+
+//delete use
+app.delete("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  try{
+    const user=await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully!");
+  }
+  catch(err){
+    res.status(400).send("Something went wrong");
+  }
+})
+
+//update 
+app.patch("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  const data=req.body;
+  try{
+    await User.findByIdAndUpdate({_id:userId},data);
+    res.send("User updated successfully!");
+  }
+  catch(err){
+    res.status(400).send("Something went wrong!")
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connected");
