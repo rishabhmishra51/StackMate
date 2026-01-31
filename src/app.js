8b8ddb8aec6041442409f4cd58b12eff9c12a892
rishabhmishra51 <rishabhmishra7570@gmail.com>
2026-01-31 13:11:20 +0530

    added patch user update and improved schema

--- src/app.js
+++ src/app.js
@@ -64,11 +64,16 @@ app.patch("/user",async(req,res)=>{
   const userId=req.body.userId;
   const data=req.body;
   try{
-    await User.findByIdAndUpdate({_id:userId},data);
+   const user = await User.findByIdAndUpdate({_id:userId},data,{
+    returnDocument:'before',
+     runValidators:true,
+    })
+    console.log(user);    
     res.send("User updated successfully!");
+   
   }
   catch(err){
-    res.status(400).send("Something went wrong!")
+    res.status(400).send("Update failed!",err.message)
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,62 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-     firstName:{
-          type:String
-     },
-     lastName:{
-          type:String
-     },
-     emailId:{
-          type:String
-     },
-     password:{
-          type:String
-     },
-     age:{
-          type:Number
-     },
-     gender:{
-          type:String
-     }
-});
-const User = mongoose.model("User",userSchema);
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+  },
+
+  lastName: {
+    type: String,
+  },
+
+  emailId: {
+    type: String,
+    lowercase: true,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  age: {
+    type: Number,
+    min: 18,
+  },
+
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender data is not valid");
+      }
+    },
+  },
+
+  photoUrl: {
+    type: String,
+    default: "https://geographyandyou.com/images/user-profile.png",
+  },
+
+  about: {
+    type: String,
+    default: "Hey there! I am using StackMate",
+  },
+  skills:{
+     type:[String],
+  },
+},
+{
+  timestamps:true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
